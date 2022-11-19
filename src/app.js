7d47b655488c893825e287d9017b1f8c7f3d2300
himanshu kabra <himanshukabra2212@gmail.com>
2022-11-20 00:23:27 +0530

    setting env -h

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const env = require("dotenv");
 const hbs = require('hbs')
 require("./db/connection");
 const bodyparser = require("body-parser");
@@ -9,7 +10,7 @@ const path = require('path')
 const {PORT} = require("../ignore")
 
 
-const port = PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
